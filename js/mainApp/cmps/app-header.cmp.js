53996febd183336e29e9decd61d3df0d8d0942c6
Ella <ella.brazlev@gmail.com>
2020-02-26 17:21:09 +0200

    navbar

--- js/mainApp/cmps/app-header.cmp.js
+++ js/mainApp/cmps/app-header.cmp.js
@@ -1,18 +1,23 @@
 export default {
     template: `
-        <div class="app-header">
-            <router-link to="/" exact>
-                Home
-            </router-link>
-            <router-link to="/about" exact>
-                About
-            </router-link>
-            <router-link to="/noteApp" exact>
-                MissKeep
-            </router-link>
-            <router-link to="/emailApp" exact>
-                MisterEmail
-            </router-link>
-        </div>
+        <section class="app-header">
+            <div class="header-container flex space-between align-center container">
+                <span class="logo">Appsus</span>
+                <nav>
+                    <router-link to="/" class="link" exact>
+                        Home
+                    </router-link>
+                    <router-link to="/about" class="link" exact>
+                        About
+                    </router-link>
+                    <router-link to="/noteApp" class="link" exact>
+                        MissKeep
+                    </router-link>
+                    <router-link to="/emailApp" class="link" exact>
+                        MisterEmail
+                    </router-link>
+                </nav>
+            </div>
+        </section>
     `
 }
