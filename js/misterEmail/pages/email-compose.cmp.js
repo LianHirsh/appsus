41e6b23713cc8715da304ef6c220f6527a0b8626
Ella <ella.brazlev@gmail.com>
2020-02-26 21:06:24 +0200

    compose

--- js/misterEmail/pages/email-compose.cmp.js
+++ js/misterEmail/pages/email-compose.cmp.js
@@ -1,7 +1,40 @@
+import { emailService } from '../services/email.service.js';
+
 export default {
     template: `
     <section class="email-compose">
-        <h1>hi</h1>
+        <div class="form-msg flex column">
+            <div class="form-title">New Message</div>
+
+            <form class="flex column" @submit.prevent="addEmail">
+                <input type="email" name="cc" placeholder="To:" v-model="email.to"/>
+
+                <input type="email" name="bcc" placeholder="Cc:" v-model="email.cc" />
+
+                <input type="email" name="to" placeholder="Bcc:" v-model="email.bcc" />
+
+                <input type="text" name="subject" placeholder="Subject:" v-model="email.subject" />
+
+                <textarea name="body" v-model="email.body"></textarea>
+
+                <button>Send</button>
+            </form>
+
+            <button>Remove</button>
+        </div>
     </section>
-    `
+    `,
+    data() {
+        return {
+            email: {}
+        }
+    },
+    methods: {
+        addEmail() {
+            emailService.addEmail(this.mail)
+                .then(() => {
+                    this.$router.push('/emailApp')
+                })
+        },
+    }
 }
