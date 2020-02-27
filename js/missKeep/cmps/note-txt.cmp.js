e7af5f263281fd89d87da8ca31cf88678fb82c18
Ella <ella.brazlev@gmail.com>
2020-02-27 16:51:32 +0200

    notes

--- js/missKeep/cmps/note-txt.cmp.js
+++ js/missKeep/cmps/note-txt.cmp.js
@@ -1,7 +1,14 @@
 export default {
     template: `
         <section class="note-txt">
-            <p>{{}}</p>
+            <div class="note-txt-content">
+                <p>{{text}}</p>
+            </div>
+            <div class="flex">
+                <span class="fas fa-font visible"></span>
+                <div class="toolbar"></div>
+            </div>
         </section>
-    `
+    `,
+    props: ['text']
 }
--- js/missKeep/cmps/note-video.cmp.js
+++ js/missKeep/cmps/note-video.cmp.js
@@ -1,6 +1,16 @@
 export default {
     template: `
         <section class="note-video">
+        <div class="note-video-content">
+            <video width="200" height="150" controls>
+                <source :src="video.info.url" title="video.info.title" type="video/mp4">
+            </video>
+        </div>
+            <div class="flex">
+                <span class="fab fa-youtube visible"></span>
+                <div class="toolbar"></div>
+            </div>
         </section>
-    `
+    `,
+     props: ['video']
 }
