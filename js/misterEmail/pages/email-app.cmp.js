02156b327ccff35f78e36160d24060429f22daed
Ella <ella.brazlev@gmail.com>
2020-02-27 08:53:51 +0200

    main app

--- js/misterEmail/pages/email-app.cmp.js
+++ js/misterEmail/pages/email-app.cmp.js
@@ -2,9 +2,9 @@ import emailNavbar from '../cmps/email-navbar.cmp.js';
 
 export default {
     template: `
-        <section class="email-app">
-            <email-navbar></email-navbar>
-            <router-view></router-view>
+        <section class="email-app container">
+            <email-navbar class="sidebar"></email-navbar>
+            <router-view class="main"></router-view>
         </section>
     `,
     components: {
