f929b4fca7b5fb80755ce6b66ff4658bc5ad9fd5
Ella <ella.brazlev@gmail.com>
2020-02-29 16:00:16 +0200

    email navbar

--- js/misterEmail/cmps/email-navbar.cmp.js
+++ js/misterEmail/cmps/email-navbar.cmp.js
@@ -4,22 +4,29 @@ export default {
     template: `
     <section class="side-navbar">
          <nav class="flex column">
-            <router-link to="/emailApp/compose" exact>
+            <router-link to="/emailApp/compose" class="compose" exact>
+                <span class="fas fa-plus"></span>
                 Compose
             </router-link>
             <router-link to="/emailApp/emailList/inbox" class="flex" exact>
-                <div>Inbox &nbsp</div> <email-status class="unreadCount"></email-status>
+                <span class="fas fa-inbox"></span>
+                <div>Inbox &nbsp</div>
+                <email-status class="unreadCount"></email-status>
             </router-link>
             <router-link to="/emailApp/emailList/starred" exact>
+                <span class="fas fa-star"></span>
                 Starred
             </router-link>
             <router-link to="/emailApp/emailList/snoozed" exact>
+                <span class="far fa-clock"></span>
                 Snoozed
             </router-link>
             <router-link to="/emailApp/emailList/sentMail" exact>
+                <span class="fas fa-share-square"></span>
                 Sent Mail
             </router-link>
             <router-link to="/emailApp/emailList/drafts" exact>
+                <span class="far fa-file"></span>
                 Drafts
             </router-link>
         </nav>
